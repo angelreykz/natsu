076339805d92e774e49c7aa6339044996a89554a
reykz <nasoki.0802@gmail.com>
2026-07-27 09:59:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    botName: "Natsukashii",
+    ownerName: "Reykz",
+    ownerNumber: "5511999999999", // Coloque seu número com DDD (ex: 55 + DDD + Número)
+    prefix: "."
+};
